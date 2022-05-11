a7d39297588e01a1c2ff5d6a050339b2069faf68
Tomoyuki Sakurai <y@trombik.org>
2022-05-11 19:16:25 +0700

    ci: add more tests

--- spec/serverspec/fab/default_spec.rb
+++ spec/serverspec/fab/default_spec.rb
@@ -2,6 +2,46 @@
 
 require_relative "../spec_helper"
 
+workers = %w[
+  app
+  worker
+]
+
+services = %w[
+  postgresql
+  redis
+  supervisord
+]
+
+ports = [
+  80,
+  5000,
+  5432,
+  6379,
+]
+
 describe command "echo" do
   its(:exit_status) { should eq 0 }
+end
+
+describe command "supervisorctl status" do
+  its(:exit_status) { should eq 0 }
+  workers.each do |worker|
+    its(:stderr) { should eq "" }
+    its(:stdout) { should match(/^#{worker}\s+RUNNING\s+/) }
+  end
+end
+
+
+services.each do |s|
+  describe service(s) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
+ports.each do |p|
+  describe port(p) do
+    it { should be_listening }
+  end
 end
