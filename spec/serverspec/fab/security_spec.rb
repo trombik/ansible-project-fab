5f02e73f52758966927318eb5d278e7971a51222
Tomoyuki Sakurai <y@trombik.org>
2022-05-19 09:43:15 +0700

    bugfix: rubocop

--- spec/serverspec/fab/security_spec.rb
+++ spec/serverspec/fab/security_spec.rb
@@ -3,7 +3,7 @@
 require_relative "../spec_helper"
 
 describe "haproxy ACLs" do
-  ["http", "https"].each do |scheme|
+  %w[http https].each do |scheme|
     describe command "curl --verbose --insecure --header 'Host: nosuch.host.mkrsgh.org' #{scheme.shellescape}://127.0.0.1/" do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should match(/#{Regexp.escape("HTTP/1.1 403")}/) }
